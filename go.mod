1426282a98d55dc8d7b3d01cc7e27fd8dff63782
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-10-29 19:28:45 +0530

    update go mod

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/theritikchoure/logx
 
-go 1.21.0
+go 1.20.0
--- logx_test.go
+++ logx_test.go
@@ -2,96 +2,39 @@ package logx
 
 import (
 	"bytes"
-	"log"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestLog(t *testing.T) {
-	t.Run("Test log with coloring enabled", func(t *testing.T) {
-		ColoringEnabled = true
-		expected := "\x1b[31m\x1b[42mTest Message\x1b[0m\n"
-		buf := new(bytes.Buffer)
-		log.SetOutput(buf)
+func captureStdout(f func()) string {
+	old := os.Stdout // Keep a copy of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
 
-		Log("Test Message", FGRED, BGGREEN)
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
 
-		if got := buf.String(); got != expected {
-			t.Errorf("Expected: %v, Got: %v", expected, got)
-		}
-	})
+	f()
 
-	// t.Run("Test log with coloring disabled", func(t *testing.T) {
-	// 	ColoringEnabled = false
-	// 	expected := "Test Message\n"
-	// 	buf := new(bytes.Buffer)
-	// 	log.SetOutput(buf)
-
-	// 	Log("Test Message", FGRED, BGGREEN)
-
-	// 	if got := buf.String(); got != expected {
-	// 		t.Errorf("Expected: %v, Got: %v", expected, got)
-	// 	}
-	// })
+	w.Close()
+	os.Stdout = old
+	out := <-outC
+	return out
 }
 
-// func TestLogf(t *testing.T) {
-// 	expected := "\x1b[31m\x1b[42mFormatted Message: 42\x1b[0m\n"
-// 	buf := new(bytes.Buffer)
-// 	log.SetOutput(buf)
+func TestLog(t *testing.T) {
+	t.Parallel()
+	expected := FGRED + BGCYAN + "Test Log" + RESET + "\n"
+	output := captureStdout(func() {
+		Log("Test Log", FGRED, BGCYAN)
+	})
 
-// 	Logf("Formatted Message: %d", FGRED, BGGREEN, 42)
-
-// 	if got := buf.String(); got != expected {
-// 		t.Errorf("Expected: %v, Got: %v", expected, got)
-// 	}
-// }
-
-// func TestLogWithLevel(t *testing.T) {
-// 	t.Run("Test log with INFO level", func(t *testing.T) {
-// 		expected := "\x1b[44mLog message with INFO level\x1b[0m\n"
-// 		buf := new(bytes.Buffer)
-// 		log.SetOutput(buf)
-
-// 		LogWithLevel("Log message with INFO level", "INFO")
-
-// 		if got := buf.String(); got != expected {
-// 			t.Errorf("Expected: %v, Got: %v", expected, got)
-// 		}
-// 	})
-
-// 	t.Run("Test log with WARNING level", func(t *testing.T) {
-// 		expected := "\x1b[43mLog message with WARNING level\x1b[0m\n"
-// 		buf := new(bytes.Buffer)
-// 		log.SetOutput(buf)
-
-// 		LogWithLevel("Log message with WARNING level", "WARNING")
-
-// 		if got := buf.String(); got != expected {
-// 			t.Errorf("Expected: %v, Got: %v", expected, got)
-// 		}
-// 	})
-// }
-
-// func TestLogM(t *testing.T) {
-// 	expected := "\x1b[31m\x1b[42mTest\x1b[0m \x1b[34m\x1b[43mMultiple\x1b[0m \x1b[32m\x1b[44mMessages\x1b[0m\n"
-// 	buf := new(bytes.Buffer)
-// 	log.SetOutput(buf)
-
-// 	LogM([]string{"Test", "Multiple", "Messages"}, FGRED, BGGREEN, FGBLUE, BGYELLOW, FGGREEN, BGBLUE)
-
-// 	if got := buf.String(); got != expected {
-// 		t.Errorf("Expected: %v, Got: %v", expected, got)
-// 	}
-// }
-
-// func TestLogWithTimestamp(t *testing.T) {
-// 	expected := "2023-01-14 15:04:05 \x1b[31m\x1b[42mTimestamped Message\x1b[0m\n"
-// 	buf := new(bytes.Buffer)
-// 	log.SetOutput(buf)
-
-// 	LogWithTimestamp("Timestamped Message", FGRED, BGGREEN)
-
-// 	if got := buf.String(); got != expected {
-// 		t.Errorf("Expected: %v, Got: %v", expected, got)
-// 	}
-// }
+	if output != expected {
+		t.Errorf("Log output: got %s, expected %s", output, expected)
+	}
+}
